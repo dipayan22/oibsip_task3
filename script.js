a23dda68712acae3f7ab3cdda8d3d298b24ad425
Dipayan Majumder <110282003+dipayan22@users.noreply.github.com>
2022-12-21 09:15:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var cel = document.getElementById("cel")
+var feh = document.getElementById("feh")
+var kel = document.getElementById("kel")
+
+cel.addEventListener('input' , function(){
+    let c = this.value ;
+    let f = (c*(9/5))+32;
+    
+    if(!Number.isInteger(f)){
+        f= f.toFixed(4);
+    }
+    feh.value=f;
+    
+})
+
+feh.addEventListener('input' , function(){
+    let f = this.value;
+    let c = (f-32)*(5/9);
+    if(!Number.isInteger(c)){
+        c= c.toFixed(4);
+    }
+    cel.value=c;
+})
+
